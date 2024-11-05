753d1be6c7a9912e7b0c106ae69bcf09282fe390
nrastija <niko.rastija@gmail.com>
2024-11-05 12:14:47 +0100

    Dodan XML u AndroidManifest i kreirana ShopActivity klasa za pozadinsku logiku #13

---
+++ Software/app/src/main/java/com/example/pawpal/f12_shop/ShopActivity.kt
@@ -0,0 +1,14 @@
+package com.example.pawpal.f12_shop
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pawpal.R
+
+class ShopActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.f12_layout_shop)
+
+        // Ovdje postavite RecyclerView adapter i druge elemente za trgovinu
+    }
+}
