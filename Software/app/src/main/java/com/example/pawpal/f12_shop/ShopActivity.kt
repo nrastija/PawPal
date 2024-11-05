843c6bd9604b3b59f549fa1d37cce015a2e4b46a
Niko <niko.rastija@gmail.com>
2024-11-05 12:53:37 +0100

    Implementirana logika prijelaza na stranicu za web shop putem hamburger menija (f12_layout_shop.xml) #13

--- Software/app/src/main/java/com/example/pawpal/f12_shop/ShopActivity.kt
+++ Software/app/src/main/java/com/example/pawpal/f12_shop/ShopActivity.kt
@@ -2,13 +2,23 @@ package com.example.pawpal.f12_shop
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.pawpal.R
+import com.example.pawpal.main.BaseActivity
+import com.google.android.material.navigation.NavigationView
 
-class ShopActivity : AppCompatActivity() {
+class ShopActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.f12_layout_shop)
 
-        // Ovdje postavite RecyclerView adapter i druge elemente za trgovinu
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+
+        setupHamburgerMenu(drawerLayout, toolbar, navView)
     }
 }
--- Software/app/src/main/java/com/example/pawpal/main/MainActivity.kt
+++ Software/app/src/main/java/com/example/pawpal/main/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.pawpal.main
 
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -10,9 +9,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.example.pawpal.R
 import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
-
-    lateinit var toggle: ActionBarDrawerToggle
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,33 +21,7 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
         val navView: NavigationView = findViewById(R.id.nav_view)
 
-        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
-
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-
-        toggle.isDrawerIndicatorEnabled = true
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        // TODO -> Issue #15 - Hamburger menu, task 7
-        navView.setNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.nav_home -> Toast.makeText(this, "Home clicked", Toast.LENGTH_SHORT).show()
-                R.id.nav_profile -> Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
-                R.id.nav_finance -> Toast.makeText(this, "Finance clicked", Toast.LENGTH_SHORT).show()
-                R.id.nav_reservations -> Toast.makeText(this, "Reservations clicked", Toast.LENGTH_SHORT).show()
-                R.id.nav_veterinar -> Toast.makeText(this, "Veterinar clicked", Toast.LENGTH_SHORT).show()
-                R.id.nav_spa -> Toast.makeText(this, "Spa clicked", Toast.LENGTH_SHORT).show()
-                R.id.nav_school -> Toast.makeText(this, "School clicked", Toast.LENGTH_SHORT).show()
-                R.id.nav_adoption -> Toast.makeText(this, "Adoption clicked", Toast.LENGTH_SHORT).show()
-                R.id.nav_lost_dogs -> Toast.makeText(this, "Lost dogs clicked", Toast.LENGTH_SHORT).show()
-                R.id.nav_shop -> Toast.makeText(this, "Shop clicked", Toast.LENGTH_SHORT).show()
-            }
-            drawerLayout.closeDrawers()
-            true
-        }
+        setupHamburgerMenu(drawerLayout, toolbar, navView)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
