b8b0b0de5fd905132d14f8d141d9ed6258cc56d4
pskoko22 <pskoko22@student.foi.hr>
2024-11-07 16:59:13 +0100

    Implementiran kalendar za odabir datuma rezervacije #5

--- Software/app/src/main/java/com/example/pawpal/f04_veterinar/RezervacijaVeterinaraActivity.kt
+++ Software/app/src/main/java/com/example/pawpal/f04_veterinar/RezervacijaVeterinaraActivity.kt
@@ -1,13 +1,19 @@
 package com.example.pawpal.f04_veterinar
 
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.pawpal.R
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class RezervacijaVeterinaraActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +22,21 @@ class RezervacijaVeterinaraActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val calendar = findViewById<CalendarView>(R.id.calendar)
+        val dateText = findViewById<TextView>(R.id.dateText)
+
+        calendar.minDate = System.currentTimeMillis()
+        calendar.setOnDateChangeListener{ _, year, month, dayOfMonth->
+            val selectedDate = Calendar.getInstance()
+            selectedDate.set(year, month, dayOfMonth)
+
+
+            val formatirajDatum = SimpleDateFormat("EEE, MMM, d, yyyy", Locale.getDefault())
+            val formatiranDatum = formatirajDatum.format(selectedDate.time)
+
+            dateText.text = "Odabran datum: $formatiranDatum"
         }
     }
 }
