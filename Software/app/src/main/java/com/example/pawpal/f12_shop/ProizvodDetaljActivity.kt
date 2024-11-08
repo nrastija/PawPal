8b0aa4dd6db5ac3cfd0760775c87f39e7a004ab7
Niko <niko.rastija@gmail.com>
2024-11-08 14:33:37 +0100

    Implementirana logika odabira količine proizvoda putem spinnera i redizajniran XML za detalje proizvoda #13

--- Software/app/src/main/java/com/example/pawpal/f12_shop/ProizvodDetaljActivity.kt
+++ Software/app/src/main/java/com/example/pawpal/f12_shop/ProizvodDetaljActivity.kt
@@ -1,12 +1,17 @@
 package com.example.pawpal.f12_shop
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.pawpal.R
 
 class ProizvodDetaljActivity : AppCompatActivity() {
@@ -14,6 +19,9 @@ class ProizvodDetaljActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.f12_product_detail)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         // Pronalazak view elemenata
         val slikaProizvoda: ImageView = findViewById(R.id.slikaProizvoda)
@@ -36,8 +44,30 @@ class ProizvodDetaljActivity : AppCompatActivity() {
         slikaProizvoda.setImageResource(R.drawable.test_slika)
 
 
+        //adapter za spinner
+        val kolicinaList = listOf("1", "2", "3", "4", "5")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, kolicinaList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerKolicina.adapter = adapter
+
         gumbDodajUKosaricu.setOnClickListener {
             Toast.makeText(this, "Pritisnut gumb dodaj u košaricu", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean { // kreiranje return gumba
+        menuInflater.inflate(R.menu.f12_menu_return, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean { // funkcija obrade klika na return gumb
+        return when (item.itemId) {
+            R.id.return_icon -> {
+                val intent = Intent(this, ShopActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
