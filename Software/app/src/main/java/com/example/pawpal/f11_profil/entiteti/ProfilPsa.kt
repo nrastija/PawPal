be3b7861658ec9ddbc8e5c9c1957c39b2890af92
nvugrinec22 <nvugrinec22@student.foi.hr>
2024-11-12 00:39:28 +0100

    Dodan ProfilPsa.kt #12

---
+++ Software/app/src/main/java/com/example/pawpal/f11_profil/entiteti/ProfilPsa.kt
@@ -0,0 +1,9 @@
+package com.example.pawpal.f11_profil.entiteti
+
+data class ProfilPsa(
+    val ime: String = "",
+    val dob: String = "",
+    val pasmina: String = "",
+    val spol: String = "",
+    val kilaza: String = ""
+)
