415432db9623f80ae161ea2e6cd701b08d23c701
Niko <niko.rastija@gmail.com>
2024-11-15 19:14:04 +0100

    Dodana klasa KosaricaManager koja ima implementiranu logiku za proizvode kosarice (privremeno lista dok ne uspostavimo ROOM BP) #13

---
+++ Software/app/src/main/java/com/example/pawpal/f12_shop/KosaricaManager.kt
@@ -0,0 +1,33 @@
+package com.example.pawpal.f12_shop
+
+import com.example.pawpal.f12_shop.entiteti.Proizvod
+
+object KosaricaManager {
+    private val kosarica: MutableList<Proizvod> = mutableListOf()
+
+    fun dodajProizvodLista(proizvod: Proizvod) {
+        kosarica.add(proizvod)
+    }
+    fun dohvatiProizvodeLista(): List<Proizvod> {
+        return kosarica
+    }
+
+    fun obrisiProizvodLista(proizvod: Proizvod) {
+        kosarica.remove(proizvod)
+    }
+
+    fun povecajKolicinuList(proizvod: Proizvod) {
+        proizvod.kolicina++;
+    }
+
+    fun smanjiKolicinuList(proizvod: Proizvod) {
+        proizvod.kolicina--;
+        if (proizvod.kolicina == 0){
+            obrisiProizvodLista(proizvod);
+        }
+    }
+
+    fun isprazniKosaricuLista() {
+        kosarica.clear()
+    }
+}
