07c38dbe7aa3ca446d6d6e7205ec8f4c3c21d642
mvukovic22 <mvukovic22@student.foi.hr>
2024-11-15 13:37:02 +0100

    Kreirana klasa za brisanje i deaktivaciju računa. Implementirana Odjava i Deaktivacija. #2

---
+++ Software/app/src/main/java/com/example/pawpal/f01_autorizacija/BrisanjeDeaktivacijaActivity.kt
@@ -0,0 +1,97 @@
+package com.example.pawpal.f01_autorizacija
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.pawpal.R
+import com.example.pawpal.main.BaseActivity
+import com.google.android.material.navigation.NavigationView
+
+class BrisanjeDeaktivacijaActivity : BaseActivity() {
+
+    private lateinit var sharedPreferences: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.f01_brisanjelayout)
+
+        sharedPreferences = getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbarBrisanje)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayoutBrisanje)
+        val navView: NavigationView = findViewById(R.id.navBrisanje)
+        setupHamburgerMenu(drawerLayout, toolbar, navView)
+
+        val btnBrisanje: Button = findViewById(R.id.btnBrisanjeProfila)
+        btnBrisanje.setOnClickListener {
+            showBrisanjeDialog()
+        }
+        val btnOdjava: Button = findViewById(R.id.btnOdjava)
+        btnOdjava.setOnClickListener {
+            showOdjavaDialog()
+        }
+    }
+
+    private fun showBrisanjeDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Jeste li sigurni da želite deaktivirati račun?")
+
+        builder.setPositiveButton("Da") { dialog, _ ->
+            deaktivacija()
+            dialog.dismiss()
+        }
+        builder.setNegativeButton("Ne") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.create().show()
+    }
+
+    private fun showOdjavaDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Jeste li sigurni da se želite odjaviti?")
+
+        builder.setPositiveButton("Da") { dialog, _ ->
+            odjava()
+            dialog.dismiss()
+        }
+        builder.setNegativeButton("Ne") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.create().show()
+    }
+
+    private fun deaktivacija() {
+        val editor = sharedPreferences.edit()
+        editor.remove("korisnikIme")
+        editor.remove("korisnikPrezime")
+        editor.remove("korisnikEmail")
+        editor.remove("korisnikKorime")
+        editor.remove("korisnikLozinka")
+        editor.apply()
+
+        Toast.makeText(this, "Uspješno ste obrisali račun.", Toast.LENGTH_SHORT).show()
+
+
+        val intent = Intent(this, PrijavaRegistracijaActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun odjava(){
+
+        Toast.makeText(this, "Odjavljeni ste.", Toast.LENGTH_SHORT).show()
+
+        val intent = Intent(this, PrijavaRegistracijaActivity::class.java)
+        startActivity(intent)
+        finish()
+
+    }
+}
