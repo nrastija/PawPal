acde80d1d15035b52f090faaf3bbb85ac17e875a
nrastija <niko.rastija@gmail.com>
2024-12-23 14:10:44 +0100

    Refakturiran MainActivity tako da vise ne nasljeduje BaseActivity #13

--- Software/app/src/main/java/com/example/pawpal/main/MainActivity.kt
+++ Software/app/src/main/java/com/example/pawpal/main/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.example.pawpal.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.example.pawpal.R
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.pawpal.f04_veterinar.odabirVeterinaraActivity
+import com.example.pawpal.f11_profil.ProfilKorisnikaActivity
+import com.example.pawpal.ui.ShopActivity
+import com.example.pawpal.ui.ShopFragment
 import com.google.android.material.navigation.NavigationView
 
-class MainActivity : BaseActivity() {
+class MainActivity : AppCompatActivity(){
+    open lateinit var toggle: ActionBarDrawerToggle
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun  onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showImagesForMain();
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
@@ -19,4 +33,67 @@ class MainActivity : BaseActivity() {
 
         setupHamburgerMenu(drawerLayout, toolbar, navView)
     }
+
+    fun setupHamburgerMenu(drawerLayout: DrawerLayout, toolbar: Toolbar, navView: NavigationView) {
+        setSupportActionBar(toolbar)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        toggle.isDrawerIndicatorEnabled = true
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_profile -> {
+                    val intent = Intent(this, ProfilKorisnikaActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_finance -> Toast.makeText(this, "Finance clicked", Toast.LENGTH_SHORT).show()
+                R.id.nav_reservations -> Toast.makeText(this, "Reservations clicked", Toast.LENGTH_SHORT).show()
+                R.id.nav_veterinar -> {
+                    val intent = Intent(this, odabirVeterinaraActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_spa -> Toast.makeText(this, "Spa clicked", Toast.LENGTH_SHORT).show()
+                R.id.nav_school -> Toast.makeText(this, "School clicked", Toast.LENGTH_SHORT).show()
+                R.id.nav_adoption -> Toast.makeText(this, "Adoption clicked", Toast.LENGTH_SHORT).show()
+                R.id.nav_lost_dogs -> Toast.makeText(this, "Lost dogs clicked", Toast.LENGTH_SHORT).show()
+                R.id.nav_shop -> {
+                    navigateToFragment(ShopFragment())
+                }
+            }
+            drawerLayout.closeDrawers()
+            true
+        }
+    }
+
+    fun navigateToFragment(fragment: Fragment) {
+        supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+
+        removeImagesForFragment();
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+
+    private fun removeImagesForFragment() {
+        findViewById<ImageView>(R.id.imageView2).visibility = View.GONE
+        findViewById<ImageView>(R.id.imageView7).visibility = View.GONE
+    }
+
+    private fun showImagesForMain(){
+        findViewById<ImageView>(R.id.imageView2).visibility = View.VISIBLE
+        findViewById<ImageView>(R.id.imageView7).visibility = View.VISIBLE
+    }
+
 }
+
+
+
