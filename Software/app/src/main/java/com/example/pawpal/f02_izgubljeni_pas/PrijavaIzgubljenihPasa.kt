89edb1423a7dfb396ff6e11a43484ef1f8e3b2f2
pskoko22 <pskoko22@student.foi.hr>
2024-12-23 20:32:19 +0100

    Kreirana grana i glavna klasa za funkcionalnost F02 #3

---
+++ Software/app/src/main/java/com/example/pawpal/f02_izgubljeni_pas/PrijavaIzgubljenihPasa.kt
@@ -0,0 +1,21 @@
+package com.example.pawpal.f02_izgubljeni_pas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.pawpal.R
+
+class PrijavaIzgubljenihPasa : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_prijava_izgubljenih_pasa)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
