4cd9c395da39906d387d00caf1cb5963cb68ed92
nrastija <niko.rastija@gmail.com>
2024-12-30 17:12:22 +0100

    Slozena logika dohvacanja ukupne sume kosarice pomocu SQL upita u KosaricaProizvod #13

--- Software/app/src/main/java/com/example/pawpal/ui/KosaricaFragment.kt
+++ Software/app/src/main/java/com/example/pawpal/ui/KosaricaFragment.kt
@@ -129,8 +129,11 @@ class KosaricaFragment : Fragment(), DatabaseConsumer {
 
     private fun azurirajUkupnuCijenu() {
         val ukupnaCijena = database.kosaricaProizvodQueries.dohvatiUkupnuCijenuZaKosaricu(kosaricaID).executeAsOneOrNull()
-        //val zaokruzenaCijena = BigDecimal(ukupnaCijena).setScale(2, RoundingMode.HALF_UP).toDouble()
-        ukupnaCijenaLabel.text = "Ukupna cijena: $ukupnaCijena €"
+        val zaokruzenaCijena = ukupnaCijena?.SUM?.let { BigDecimal(it).setScale(2, RoundingMode.HALF_UP).toDouble() }
+
+        if (ukupnaCijena != null) {
+            ukupnaCijenaLabel.text = "Ukupna cijena: ${zaokruzenaCijena} €"
+        }
     }
 
     private fun updateProizvodList(proizvodi: List<DohvatiProizvodeZaKosaricu>) {
