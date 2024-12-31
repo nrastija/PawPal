e02120d458c097d45528406c5b280e2f3b2870b5
mvukovic22 <mvukovic22@student.foi.hr>
2024-12-31 01:06:48 +0100

    Kreirana DataSource i implementacija za udomljavanje psa #10

---
+++ Software/app/src/main/java/com/example/pawpal/data/datasource/PasUdomljavanjeDataSource.kt
@@ -0,0 +1,25 @@
+package com.example.pawpal.data.datasource
+
+import appdatabase.Pasudomljavanje
+import kotlinx.coroutines.flow.Flow
+
+interface PasUdomljavanjeDataSource {
+
+    suspend fun dohvatiPsaPoImenu (ime:String) : Pasudomljavanje?
+    suspend fun dohvatiPsaPoID (pasudomljavanjeID: Long) : Pasudomljavanje?
+
+    fun dohvatiSvePse(): Flow<List<Pasudomljavanje>>
+
+    suspend fun dodajPasUdomljavanje(
+        ime: String,
+        dob: Long,
+        spol:String ,
+        opis: String,
+        datumRodenja: String,
+        kilaza: Double,
+        pasmina: String,
+        dodatneInfo: String,
+        cijepiva: String,
+        imageUrl: String
+    )
+}
