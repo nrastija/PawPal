18c8f64c7d8e82c70a0fb0a1e131ca467f8c8669
pskoko22 <pskoko22@student.foi.hr>
2025-01-02 23:08:01 +0100

    Implementiran dizajn stranice za odabir prijave ili pregleda izgubljenih pasa #3

---
+++ Software/app/src/main/java/com/example/pawpal/f02_izgubljeni_pas/OdabirPrijaveIliPregledaPsaActivity.kt
@@ -0,0 +1,21 @@
+package com.example.pawpal.f02_izgubljeni_pas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.pawpal.R
+
+class OdabirPrijaveIliPregledaPsaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_odabir_prijave_ili_pregleda_psa)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
