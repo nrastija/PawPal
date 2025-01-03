8c6d256edd88f696da6636e028d3d951f5c26f9f
mvukovic22 <mvukovic22@student.foi.hr>
2025-01-03 14:13:39 +0100

    Promjene zbog mergeanja #2

--- Software/app/src/main/java/com/example/pawpal/main/BaseActivity.kt
+++ Software/app/src/main/java/com/example/pawpal/main/BaseActivity.kt
@@ -10,8 +10,6 @@ import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.pawpal.R
 import com.example.pawpal.f04_veterinar.odabirVeterinaraActivity
-
-import com.example.pawpal.f11_profil.ProfilKorisnikaActivity
 import com.google.android.material.navigation.NavigationView
 
 open class BaseActivity : AppCompatActivity() {
