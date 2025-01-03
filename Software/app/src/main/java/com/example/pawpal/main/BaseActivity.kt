057abf78f7c3716742a4ea1a1c57c220a55328f9
mvukovic22 <mvukovic22@student.foi.hr>
2025-01-03 10:14:27 +0100

    Sitne promjene #2

--- Software/app/src/main/java/com/example/pawpal/main/BaseActivity.kt
+++ Software/app/src/main/java/com/example/pawpal/main/BaseActivity.kt
@@ -10,7 +10,6 @@ import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.pawpal.R
 import com.example.pawpal.f04_veterinar.odabirVeterinaraActivity
-import com.example.pawpal.f09_udomljavanje.UdomljavanjeActivity
 import com.example.pawpal.f11_profil.ProfilKorisnikaActivity
 import com.google.android.material.navigation.NavigationView
 
@@ -51,8 +50,6 @@ open class BaseActivity : AppCompatActivity() {
                 R.id.nav_spa -> Toast.makeText(this, "Spa clicked", Toast.LENGTH_SHORT).show()
                 R.id.nav_school -> Toast.makeText(this, "School clicked", Toast.LENGTH_SHORT).show()
                 R.id.nav_adoption -> {
-                    val intent = Intent(this, UdomljavanjeActivity::class.java)
-                    startActivity(intent)
                 }
                 R.id.nav_lost_dogs -> Toast.makeText(this, "Lost dogs clicked", Toast.LENGTH_SHORT).show()
                 R.id.nav_shop -> {
