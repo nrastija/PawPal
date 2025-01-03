ae8e75c5f382035f6257d26873b4c8c611260535
nvugrinec22 <nvugrinec22@student.foi.hr>
2025-01-03 13:55:42 +0100

    Ažuriran BaseActivity #8

--- Software/app/src/main/java/com/example/pawpal/main/BaseActivity.kt
+++ Software/app/src/main/java/com/example/pawpal/main/BaseActivity.kt
@@ -10,8 +10,7 @@ import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.pawpal.R
 import com.example.pawpal.f04_veterinar.odabirVeterinaraActivity
-import com.example.pawpal.f09_udomljavanje.UdomljavanjeActivity
-import com.example.pawpal.f11_profil.ProfilKorisnikaActivity
+
 import com.google.android.material.navigation.NavigationView
 
 open class BaseActivity : AppCompatActivity() {
@@ -38,10 +37,10 @@ open class BaseActivity : AppCompatActivity() {
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                 }
-                R.id.nav_profile -> {
-                    val intent = Intent(this, ProfilKorisnikaActivity::class.java)
-                    startActivity(intent)
-                }
+//                R.id.nav_profile -> {
+//                    val intent = Intent(this, ProfilKorisnikaActivity::class.java)
+//                    startActivity(intent)
+//                }
                 R.id.nav_finance -> Toast.makeText(this, "Finance clicked", Toast.LENGTH_SHORT).show()
                 R.id.nav_reservations -> Toast.makeText(this, "Reservations clicked", Toast.LENGTH_SHORT).show()
                 R.id.nav_veterinar -> {
@@ -50,10 +49,7 @@ open class BaseActivity : AppCompatActivity() {
                 }
                 R.id.nav_spa -> Toast.makeText(this, "Spa clicked", Toast.LENGTH_SHORT).show()
                 R.id.nav_school -> Toast.makeText(this, "School clicked", Toast.LENGTH_SHORT).show()
-                R.id.nav_adoption -> {
-                    val intent = Intent(this, UdomljavanjeActivity::class.java)
-                    startActivity(intent)
-                }
+                R.id.nav_adoption -> Toast.makeText(this, "Adoption clicked", Toast.LENGTH_SHORT).show()
                 R.id.nav_lost_dogs -> Toast.makeText(this, "Lost dogs clicked", Toast.LENGTH_SHORT).show()
                 R.id.nav_shop -> {
                 }
