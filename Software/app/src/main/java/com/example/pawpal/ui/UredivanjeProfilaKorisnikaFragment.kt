4fc223e801f314e55b7afc4d4f5ca4f45428f2e6
mvukovic22 <mvukovic22@student.foi.hr>
2025-01-04 14:01:27 +0100

    Uređivanje profila korisnika izmjene #2

--- Software/app/src/main/java/com/example/pawpal/ui/UredivanjeProfilaKorisnikaFragment.kt
+++ Software/app/src/main/java/com/example/pawpal/ui/UredivanjeProfilaKorisnikaFragment.kt
@@ -115,7 +115,17 @@ class UredivanjeProfilaKorisnikaFragment : Fragment(), DatabaseConsumer {
                 )
 
                 prikaziPoruku("Promjene su uspješno spremljene.")
-                parentFragmentManager.popBackStack()
+
+                val fragment = ProfilKorisnikaFragment()
+                if (fragment is DatabaseConsumer) {
+                    fragment.database = database
+                }
+
+                parentFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainer, fragment)
+                    .addToBackStack(null)
+                    .commit()
+
             } catch (e: Exception) {
                 prikaziPoruku("Greška pri spremanju podataka: ${e.message}")
             }
@@ -126,4 +136,3 @@ class UredivanjeProfilaKorisnikaFragment : Fragment(), DatabaseConsumer {
         Toast.makeText(requireContext(), poruka, Toast.LENGTH_SHORT).show()
     }
 }
-
