d8fa52033eeb4789cf878041ca72f36e8480c25e
nvugrinec22 <nvugrinec22@student.foi.hr>
2025-01-31 15:33:12 +0100

    Ažuriran NarudzbaDatasource #6

--- Software/app/src/main/java/com/example/pawpal/data/datasource/NarudzbaDataSource.kt
+++ Software/app/src/main/java/com/example/pawpal/data/datasource/NarudzbaDataSource.kt
@@ -1,11 +1,23 @@
 package com.example.pawpal.data.datasource
 
 import appdatabase.Narudzba
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import java.util.Date
 
 interface NarudzbaDataSource {
     suspend fun insertNarudzba(korisnikId: Long, ukupnaCijena: Double, datum: String, status: String, nacinPlacanja: String)
 
     suspend fun dohvatiNarudzbu(narudzbaId: Long): Narudzba?
+
+    suspend fun dohvatiUkupneTroskove(korisnikId: Long): Double
+
+
+    suspend fun dohvatiSveNarudzbeKorisnika(korisnikId: Long): List<Narudzba>
+
+    suspend fun dohvatiUkupneTroskoveSvihKorisnika(): Double
+    suspend fun dohvatiSveNarudzbe(): List<Narudzba>
+
+
 }
