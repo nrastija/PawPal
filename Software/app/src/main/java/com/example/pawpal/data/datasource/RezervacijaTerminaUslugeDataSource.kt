43c967cf0b2177115494ed933a72efad071e91eb
nvugrinec22 <nvugrinec22@student.foi.hr>
2025-01-31 15:34:19 +0100

    Ažuriran RezervacijaTerminaUslugeDataSource #6

--- Software/app/src/main/java/com/example/pawpal/data/datasource/RezervacijaTerminaUslugeDataSource.kt
+++ Software/app/src/main/java/com/example/pawpal/data/datasource/RezervacijaTerminaUslugeDataSource.kt
@@ -1,7 +1,9 @@
 package com.example.pawpal.data.datasource
 
 import appdatabase.RezervacijaTermina
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 
 interface RezervacijaTerminaUslugeDataSource {
     suspend fun dodajUslugu(
@@ -14,5 +16,8 @@ interface RezervacijaTerminaUslugeDataSource {
     suspend fun dohvatiRezervacijeZaKorisnika(korisnikID: Long): Flow<List<RezervacijaTermina>>
     suspend fun obrisiRezervacijuPoID(rezervacijaTerminID: Long)
     suspend fun dohvatiZadnjuRezervaciju(): RezervacijaTermina?
+    suspend fun dohvatiUkupneTroskoveRezervacija(korisnikID: Long): Double
+    suspend fun dohvatiRezervacijeKorisnikaSDetaljima(korisnikId: Long): List<Pair<String, Triple<Double, String, String>>>
 
+    suspend fun dohvatiUkupneTroskoveRezervacijaSvihKorisnika(): Double
 }
